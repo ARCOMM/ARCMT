00eae4b26189c6d17621fab27eac2f419a8d1def
James Kingsley <jameslkingsley@outlook.com>
2016-03-14 13:43:02 +0000

    travis stuff

--- tools/buildSandbox.py
+++ tools/buildSandbox.py
@@ -1,4 +1,15 @@
 import re
+import shutil, errno
+
+def copy_folder(src, dst):
+	try:
+		shutil.copytree(src, dst)
+	except OSError as exc:
+		if exc.errno == errno.ENOTDIR:
+			shutil.copy(src, dst)
+		else: raise
+		
+copy_folder("../ARCMF.VR", "../build/ARCMF_Sandbox.VR")
 
 # Copy ARCMF folder
 # Rename folder
