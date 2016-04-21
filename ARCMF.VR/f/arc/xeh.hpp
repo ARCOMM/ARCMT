9f240e5f511b260e4d023244d8332f1495678d41
James Kingsley <jameslkingsley@outlook.com>
2016-04-22 00:07:41 +0100

    disabled ai setup - arc_ai_xeh.pbo handles it now

--- ARCMF.VR/f/arc/xeh.hpp
+++ ARCMF.VR/f/arc/xeh.hpp
@@ -1,7 +1,7 @@
 class Extended_Init_Eventhandlers {
     class CAManBase {
         class ARCMF {
-            ARC_setupAI = "_this call ARC_fnc_setupAI";
+            //ARC_setupAI = "_this call ARC_fnc_setupAI";
             ARC_applyLoadout = "_this call ARC_fnc_applyLoadout";
             ARC_setupSandbox = "_this call ARC_fnc_setupSandbox";
         };
