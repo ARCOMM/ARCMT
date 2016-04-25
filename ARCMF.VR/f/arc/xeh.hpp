578b1a4bc975f19102bce857f0e2e92622d37ad6
James Kingsley <jameslkingsley@outlook.com>
2016-04-25 16:10:24 +0100

    ai gear fix

--- ARCMF.VR/f/arc/xeh.hpp
+++ ARCMF.VR/f/arc/xeh.hpp
@@ -1,9 +1,6 @@
 class Extended_Init_Eventhandlers {
     class CAManBase {
-        class ARCMF {
-            //ARC_setupAI = "_this call ARC_fnc_setupAI";
-            ARC_applyLoadout = "_this call ARC_fnc_applyLoadout";
-            ARC_setupSandbox = "_this call ARC_fnc_setupSandbox";
-        };
+        ARC_applyLoadout = "_this call ARC_fnc_applyLoadout";
+        ARC_setupSandbox = "_this call ARC_fnc_setupSandbox";
     };
 };
