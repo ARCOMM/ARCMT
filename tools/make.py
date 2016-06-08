f06ca656384e5efa841621957202f8080e6127c9
James Kingsley <jameslkingsley@outlook.com>
2016-06-08 19:34:56 +0100

    Shorten make.py and update .gitignore

--- tools/make.py
+++ tools/make.py
@@ -1,7 +1,2 @@
 import shutil
-
-build_dir = "../release"
-arcmf_dir = "../ARCMF.VR"
-with open('../ARCMF.VR/version.txt') as v: version = v.read()
-
-shutil.make_archive("{}/ARCMF".format(build_dir), 'zip', arcmf_dir)
+shutil.make_archive('../release/ARCMF', 'zip', '../ARCMF.VR')
