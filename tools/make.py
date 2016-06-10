c0adcdeab98d7c4ff699708d968742d107007760
James Kingsley <jameslkingsley@outlook.com>
2016-06-10 12:10:44 +0100

    Update make.py

--- tools/make.py
+++ tools/make.py
@@ -1,2 +1,20 @@
+import os
 import shutil
-shutil.make_archive('../release/ARCMF', 'zip', '../ARCMF.VR')
+
+def copy_folder(src, dst):
+	try:
+		shutil.copytree(src, dst)
+	except OSError as exc:
+		if exc.errno == errno.ENOTDIR:
+			shutil.copy(src, dst)
+		else: raise
+
+tempPath = '../release/ARCMF.VR'
+if not os.path.exists(tempPath):
+    os.makedirs(tempPath)
+
+copy_folder("../ARCMF.VR", "{}/ARCMF.VR".format(tempPath))
+
+shutil.make_archive('../release/ARCMF', 'zip', tempPath)
+
+shutil.rmtree(tempPath)
